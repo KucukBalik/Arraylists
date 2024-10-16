fe3fa02535ec9032b054d98bcf680959c3c43b9d
DERVISEMREDEMIR-STUD <g00427391@atu.ie>
2024-10-16 16:20:12 +0100

    Array created

---
+++ src/main/java/ie/atu/week5/Main.java
@@ -0,0 +1,16 @@
+package ie.atu.week5;
+
+
+import java.sql.Array;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+            int[] anArray;
+
+            anArray = new int[10];
+
+
+    }
+}
